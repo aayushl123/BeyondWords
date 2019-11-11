bf6a86bc3846d8a9d1b051ddd2555be19cfb14e7
Karan Sharma <95sharma.karan@gmail.com>
2019-11-11 17:59:11 -0500

    Added Splash Screen

--- app/src/main/java/com/beyondwords/activity/AgreementActivity.java
+++ app/src/main/java/com/beyondwords/activity/AgreementActivity.java
@@ -1,13 +1,16 @@
-package com.beyondwords;
+package com.beyondwords.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.beyondwords.R;
+
+public class AgreementActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agreement);
     }
 }
