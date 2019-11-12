026a5ecaccec287c8344db3de93b3bfe7cbf2bc5
aayushl123 <ayus.lami7@gmail.com>
2019-11-12 14:44:47 -0500

    Clicking login button goes to agreement screen
    Clicking login button goes to agreement screen

--- app/src/main/java/com/beyondwords/activity/LoginActivity.java
+++ app/src/main/java/com/beyondwords/activity/LoginActivity.java
@@ -3,7 +3,10 @@ package com.beyondwords.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.beyondwords.R;
 
@@ -13,5 +16,14 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login_btn = (Button) findViewById(R.id.bt_login);
+        login_btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, AgreementActivity.class));
+            }
+        });
     }
 }
