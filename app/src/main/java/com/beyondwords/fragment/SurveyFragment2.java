6924abf483d5c1a61ae30453bbb155b33972c0b4
Karan Sharma <95sharma.karan@gmail.com>
2019-11-13 03:18:23 -0500

    Added Survey Screen and empty subscreens

---
+++ app/src/main/java/com/beyondwords/fragment/SurveyFragment2.java
@@ -0,0 +1,49 @@
+package com.beyondwords.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.beyondwords.R;
+
+
+public class SurveyFragment2 extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_survey_fragment2, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
