ae424197d65fad115c62ac5d7c49f8eebe324488
aayushl123 <ayus.lami7@gmail.com>
2019-11-14 23:15:11 -0500

    Terms and conditions screen - final version

--- app/src/main/java/com/beyondwords/activity/AgreementActivity.java
+++ app/src/main/java/com/beyondwords/activity/AgreementActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.beyondwords.R;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class AgreementActivity extends Activity {
 
@@ -18,12 +27,37 @@ public class AgreementActivity extends Activity {
         setContentView(R.layout.activity_agreement);
         setTitle(R.string.terms_cond);
 
-        Button agreeBt = (Button) findViewById(R.id.bt_agree);
-        agreeBt.setOnClickListener(new View.OnClickListener(){
+        final Button agreeBt = (Button) findViewById(R.id.bt_agree);
+        final Button declineBt = (Button) findViewById(R.id.bt_decline);
+        CheckBox checkBox= ( CheckBox ) findViewById( R.id.chk_box);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if ( isChecked )
+                {
+                    agreeBt.setOnClickListener(new View.OnClickListener(){
+
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(AgreementActivity.this, SurveyActivity.class));
+                        }
+                    });
+                    agreeBt.setEnabled(TRUE);
+                }
+                else{
+                    agreeBt.setEnabled(FALSE);
+                }
+            }
+        });
+
+
+        declineBt.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(AgreementActivity.this, SurveyActivity.class));
+                startActivity(new Intent(AgreementActivity.this, LoginActivity.class));
             }
         });
     }
