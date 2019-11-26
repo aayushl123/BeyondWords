6e6be1e48c862d049fffedae3e3851c30d489245
Karan Sharma <95sharma.karan@gmail.com>
2019-11-26 15:07:20 -0500

    Change bottom view for submit and reaction tab

--- app/src/main/java/com/beyondwords/activity/SurveyActivity.java
+++ app/src/main/java/com/beyondwords/activity/SurveyActivity.java
@@ -1,15 +1,22 @@
 package com.beyondwords.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -38,14 +45,12 @@ public class SurveyActivity extends AppCompatActivity {
         linearLayout=findViewById(R.id.emoji);
         viewPager = findViewById(R.id.view_pager);
 
+
+
         final View submitView = getLayoutInflater().inflate( R.layout.dialog_layout_submit, null);
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Dialog dialog = new Dialog(SurveyActivity.this, android.R.style.Theme_Translucent_NoTitleBar);
-
-                dialog.setContentView(R.layout.dialog_layout_submit);
-                dialog.show();*/
                 viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
             }
 
@@ -65,12 +70,12 @@ public class SurveyActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 viewPager.setCurrentItem(viewPager.getCurrentItem()-1);
-                System.out.println(viewPager.getCurrentItem());
+              /*  System.out.println(viewPager.getCurrentItem());
                 if(viewPager.getCurrentItem()==9){
                     phase2_next.setText("Next");
                     onNextBtClick();
                 }
-
+*/
             }
         });
 
@@ -79,7 +84,7 @@ public class SurveyActivity extends AppCompatActivity {
             public void onClick(View v) {
                 viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
 
-                //Log.e("here",""+viewPager.getCurrentItem());
+              /*  //Log.e("here",""+viewPager.getCurrentItem());
                 System.out.println("here"+viewPager.getCurrentItem());
 
                 if(viewPager.getCurrentItem()==10){
@@ -96,11 +101,11 @@ public class SurveyActivity extends AppCompatActivity {
                     phase2_next.setText("Next");
                     onNextBtClick();
 
-                }
+                }*/
             }
         });
 
-        phase2_next.setOnClickListener(new View.OnClickListener() {
+       /* phase2_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
@@ -124,54 +129,86 @@ public class SurveyActivity extends AppCompatActivity {
 
                 }
             }
+        });*/
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                closeKeyboard();
+
+                System.out.println(position);
+                if(position==11){
+
+                    System.out.println("here at last");
+
+                    linearLayout.setVisibility(View.GONE);
+                    phase2_next.setVisibility(View.VISIBLE);
+
+                }
+                else {
+
+                    linearLayout.setVisibility(View.VISIBLE);
+                    phase2_next.setVisibility(View.GONE);
+
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
         });
 
-
+        onSubmitBtClick();
     }
 
-    private void onNextBtClick(){
-        if(viewPager.getCurrentItem()==10){
-            phase2_next.setText("Submit");
+    private void onSubmitBtClick(){
 
-
-            phase2_next.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(getApplicationContext(),"Response Submitted",Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-        else {
-
-            phase2_next.setText("Next");
-
-        }
 
 
         phase2_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
-                System.out.println(viewPager.getCurrentItem());
-                // Log.e("here",""+viewPager.getCurrentItem());
+                Dialog dialog = new Dialog(SurveyActivity.this, android.R.style.Theme_Translucent_NoTitleBar);
 
-                if(viewPager.getCurrentItem()==10){
-                    phase2_next.setText("Submit");
+                dialog.setContentView(R.layout.dialog_layout_submit);
+                Button btn_close=(Button)dialog.findViewById(R.id.btn_close);
+                dialog.show();
 
-
-                    phase2_next.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Toast.makeText(getApplicationContext(),"Response Submitted",Toast.LENGTH_LONG).show();
+                btn_close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            SurveyActivity.this.finishAffinity();
+                        } else{
+                            SurveyActivity.this.finish();
+                            System.exit(0);
                         }
-                    });
-                }
-                else {
+                    }
+                });
 
-                    phase2_next.setText("Next");
-
-                }
             }
         });
     }
+
+    private void closeKeyboard() {
+
+        Activity activity = this;
+
+        View view = activity.getCurrentFocus();
+
+
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+    }
+
 }
