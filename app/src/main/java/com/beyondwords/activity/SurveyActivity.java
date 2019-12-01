a123f5c8e628bbf38b2031a66e1f30c9623ba9b7
Karan Sharma <95sharma.karan@gmail.com>
2019-12-01 18:06:11 -0500

    Save database locally

--- app/src/main/java/com/beyondwords/activity/SurveyActivity.java
+++ app/src/main/java/com/beyondwords/activity/SurveyActivity.java
@@ -3,19 +3,26 @@ package com.beyondwords.activity;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -26,9 +33,13 @@ import android.widget.Toast;
 
 import com.beyondwords.R;
 import com.beyondwords.activity.adapter.ViewPagerAdapter;
+import com.beyondwords.database.CSVWriter;
 import com.beyondwords.database.UserDatabase;
 import com.beyondwords.model.PersonInfo;
 import com.google.android.material.tabs.TabLayout;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class SurveyActivity extends AppCompatActivity {
 
@@ -37,6 +48,7 @@ public class SurveyActivity extends AppCompatActivity {
      ViewPager viewPager;
      LinearLayout linearLayout;
      UserDatabase db;
+    private String TAG="Act";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,7 +159,7 @@ public class SurveyActivity extends AppCompatActivity {
                 closeKeyboard();
 
                 System.out.println(position);
-                if(position==11){
+                if(position==14){
 
                     System.out.println("here at last");
 
@@ -176,6 +188,28 @@ public class SurveyActivity extends AppCompatActivity {
 
         db=new UserDatabase(SurveyActivity.this);
        onSubmitBtClick();
+
+
+
+    }
+
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
     }
 
     private void onSubmitBtClick(){
@@ -183,33 +217,41 @@ public class SurveyActivity extends AppCompatActivity {
         phase2_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("here");
-                Dialog dialog = new Dialog(SurveyActivity.this, android.R.style.Theme_Translucent_NoTitleBar);
 
-                dialog.setContentView(R.layout.dialog_layout_submit);
-                Button btn_close=(Button)dialog.findViewById(R.id.btn_close);
-                dialog.show();
+                if (isStoragePermissionGranted()) {
+                    System.out.println("here");
+                    Dialog dialog = new Dialog(SurveyActivity.this, android.R.style.Theme_Translucent_NoTitleBar);
 
-                PersonInfo personInfo=PersonInfo.getInstance();
-                db.insertData(personInfo.getmGender(),personInfo.getmAge(), personInfo.getmCitizenShip(), personInfo.getmCountry(),
-                        personInfo.getmLanguage(), personInfo.getmEthnicity(), personInfo.getmReligion(), personInfo.getmSocio(),
-                        personInfo.getmProfessionalTraining(), personInfo.getmProfessionalStatus(), personInfo.getmOrgnaization(),
-                        personInfo.getmOrganiState(), personInfo.getmOrgnFunction(), personInfo.getmFrequency(),
-                        personInfo.getmDescription(),personInfo.getmConcern());
+                    dialog.setContentView(R.layout.dialog_layout_submit);
+                    Button btn_close = (Button) dialog.findViewById(R.id.btn_close);
+                    dialog.show();
 
-                sendEmail();
+                    PersonInfo personInfo = PersonInfo.getInstance();
+                    db.insertData(personInfo.getmGender(), personInfo.getmAge(), personInfo.getmCitizenShip(), personInfo.getmCountry(),
+                            personInfo.getmLanguage(), personInfo.getmEthnicity(), personInfo.getmReligion(), personInfo.getmSocio(),
+                            personInfo.getmProfessionalTraining(), personInfo.getmProfessionalStatus(), personInfo.getmOrgnaization(),
+                            personInfo.getmOrganiState(), personInfo.getmOrgnFunction(), personInfo.getmFrequency(),
+                            personInfo.getmDescription(), personInfo.getmConcern());
 
-                btn_close.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                            SurveyActivity.this.finishAffinity();
-                        } else{
-                            SurveyActivity.this.finish();
-                            System.exit(0);
+                    sendEmail();
+
+                    btn_close.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                                SurveyActivity.this.finishAffinity();
+                            } else {
+                                SurveyActivity.this.finish();
+                                System.exit(0);
+                            }
                         }
-                    }
-                });
+                    });
+
+
+                    exportDB();
+
+
+                }
 
             }
         });
@@ -244,5 +286,48 @@ public class SurveyActivity extends AppCompatActivity {
     }
 
 
+    private void exportDB() {
 
+        UserDatabase dbhelper = new UserDatabase(getApplicationContext());
+        File exportDir = new File(Environment.getExternalStorageDirectory(), "");
+        if (!exportDir.exists())
+        {
+            exportDir.mkdirs();
+        }
+
+        File file = new File(exportDir, "csvname.csv");
+        try
+        {
+            file.createNewFile();
+            CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+            SQLiteDatabase db = dbhelper.getReadableDatabase();
+            Cursor curCSV = db.rawQuery("SELECT * FROM userInfo",null);
+            csvWrite.writeNext(curCSV.getColumnNames());
+            while(curCSV.moveToNext())
+            {
+                //Which column you want to exprort
+                String arrStr[] ={curCSV.getString(0),curCSV.getString(1), curCSV.getString(2)};
+                csvWrite.writeNext(arrStr);
+            }
+            csvWrite.close();
+            curCSV.close();
+            System.out.println("here is the saved filee"+file.getAbsolutePath());
+        }
+        catch(Exception sqlEx)
+        {
+            Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
+        }
+        else {
+            System.out.println("Permisiion denies");
+        }
+    }
 }
